0415e2e230d9cb994b24c7c83b4ce2c13bc03882
Dharmesh Thorgavankar <dharmeshthor2000@gmail.com>
2023-06-21 12:40:14 +0530

    Initial commit

--- src/app/components/enter-ques/enter-ques.component.ts
+++ src/app/components/enter-ques/enter-ques.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators} from '@angular/forms'
+import { EnterQuesService } from 'src/app/services/enter-ques.service';
 
 @Component({
   selector: 'app-enter-ques',
-  templateUrl: './enter-ques.component.html',
+  templateUrl:'./enter-ques.component.html',
   styleUrls: ['./enter-ques.component.css']
 })
 export class EnterQuesComponent {
+  constructor(private enterQuestionService:EnterQuesService){
+    
+  }
 
   characterCount:number=500;
 
@@ -15,8 +19,20 @@ export class EnterQuesComponent {
   }
 
   addQuesForm= new FormGroup({
-    newQues: new FormControl('', [Validators.required, Validators.minLength(5)])
+    question: new FormControl('', [Validators.required, Validators.minLength(5)])
   })
+  public submitquestion(addQuesForm:FormGroup){
+    console.log(addQuesForm.value);
+    this.enterQuestionService.submitquestion(addQuesForm.value).subscribe(
+      (response:any) =>{
+        console.log("Question is submitted");
+        alert("Question submitted")
+      },(error)=>{console.log(error)}
+      
+   
+    )
+
+  }
 
 }
 
--- src/app/services/enter-ques.service.ts
+++ src/app/services/enter-ques.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnterQuesService {
+ 
+  baseUrl="http://localhost:9091"
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  
+  public submitquestion(addQuesForm:any){
+     return this.http.post(`${this.baseUrl}/question/addQuestion`,addQuesForm);
+  }
 }
